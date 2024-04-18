23a0588eb5394dfcd69bfb567b0bd3d4760be7ba
nathancalderon <nathan-calderon@hotmail.com>
2024-04-18 15:43:04 -0300

    feat(p1-pdmt): cria componente de entrada

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import TarefaEntrada from './TarefaEntrada'
 
 export default class App extends Component {
     render() {
         return(
-            <div className="container mt-5 col-sm-12 col-md-6" >
+            <div className="container mt-5 w-50" >
                 <div className="row">
-                    <h1>Hello, tarefas</h1>
+                    <TarefaEntrada/>
                 </div>
                 <div className="row">
                 </div>  
---
+++ src/components/TarefaEntrada.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class TarefaEntrada extends Component {
+  render() {
+    return (
+        <div>
+          <form className="row ">
+              <input type="text" className="form-control p-3 rounded-3 mb-2" placeholder="Digite a descrição de uma nova tarefa" />
+              <input type="submit" value="OK" className="btn btn-primary p-2 mb-4"/>
+          </form>
+        </div>
+    )
+  }
+}
