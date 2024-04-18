a679d259438688b524bcd30b7188ca0d7b6740fe
nathancalderon <nathan-calderon@hotmail.com>
2024-04-18 15:32:48 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return(
+            <div className="container mt-5 col-sm-12 col-md-6" >
+                <div className="row">
+                    <h1>Hello, tarefas</h1>
+                </div>
+                <div className="row">
+                </div>  
+            </div>
+        )
+    }
+}
